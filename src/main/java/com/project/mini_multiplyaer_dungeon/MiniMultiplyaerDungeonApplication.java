bfacf6d5fc356e27aa51eec9547160c8ee24af03
IkhwanAl <ikhwanal235@gmail.com>
2025-10-01 10:32:12 +0700

    init

---
+++ src/main/java/com/project/mini_multiplyaer_dungeon/MiniMultiplyaerDungeonApplication.java
@@ -0,0 +1,13 @@
+package com.project.mini_multiplyaer_dungeon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniMultiplyaerDungeonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniMultiplyaerDungeonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/mini_multiplyaer_dungeon/MiniMultiplyaerDungeonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.mini_multiplyaer_dungeon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniMultiplyaerDungeonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
